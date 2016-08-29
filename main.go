2983dc816a606563df84f926c42adf06d9a3bcf6
Scott Lee <scabwort@gmail.com>
2016-08-29 23:48:25 +0800

    add console input
    add bind error handle, and add console input handle

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	port := flag.Int("port", 80, "http port")
+	port := ""
+	flag.StringVar(&port, "port", "", "listen port")
 	flag.Parse()
-	fmt.Println("start http server at", *port)
-	pwd, _ := os.Getwd()
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("getwd error!", err.Error())
+		return
+	}
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(pwd))))
-	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+
+	if port != "" {
+		fmt.Println("start http server at:", port)
+		err = http.ListenAndServe(":"+port, nil)
+		if err != nil {
+			fmt.Println("bind error!", err.Error())
+		} else {
+			return
+		}
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter server port: ")
+		port, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("error!", err.Error())
+			continue
+		}
+		port = strings.TrimSpace(port)
+		fmt.Println("start http server at:", port)
+		err = http.ListenAndServe(":"+port, nil)
+		if err != nil {
+			fmt.Println("bind error!", err.Error())
+		}
+	}
 }
